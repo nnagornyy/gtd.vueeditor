f688cb8ad09a3125bfd61a04704374f03775c105
nnagornyy <n.nagornyy@gmail.com>
2020-07-24 21:57:26 +0300

    auto load chunk

--- lib/editor.php
+++ lib/editor.php
@@ -2,18 +2,55 @@
 
 namespace Gtd\VueEditor;
 
+use Bitrix\Main\Page\Asset;
+
 class Editor {
 
-    const MODULE_DIR = '/local/modules/gtd.vueeditor/';
+    const ASSET_SUB_DIR = '/app/asset/';
 
-    public function __construct()
+    private $moduleDir;
+
+    private $assetDir;
+
+    public function __construct($baseDir = "")
     {
+        if($baseDir === ""){
+            $this->moduleDir = dirname(__DIR__);
+            $this->moduleDir = str_replace($_SERVER['DOCUMENT_ROOT'],'', $this->moduleDir);
+        }
+        $this->assetDir = $this->moduleDir.self::ASSET_SUB_DIR;
     }
 
-    public static function initEditor(){
-        $asset = \Bitrix\Main\Page\Asset::getInstance();
-        $asset->addJs(self::MODULE_DIR.'app/asset/script.js');
-        $asset->addJs(self::MODULE_DIR.'app/asset/field.script.js');
+    public function initEditor(){
+        $this->loadJsFiles();
         echo '<div id="vue_editor"></div>';
+    }
+
+    private function loadJsFiles(){
+        $asset = Asset::getInstance();
+        foreach ($this->getJsFile() as $jsFileName){
+            $asset->addJs($this->assetDir.'/'.$jsFileName);
+        }
+    }
+
+    /**
+     * @return array|false
+     */
+    private function getJsFile(){
+        $assetFiles = scandir($this->getAssetDir());
+        return array_filter($assetFiles, function ($arr1){
+            return stripos($arr1,'.js');
+        });
+    }
+
+    /**
+     * @return string
+     */
+    public function getAssetDir($from_root = true)
+    {
+        if($from_root){
+            return $_SERVER['DOCUMENT_ROOT'].$this->assetDir;
+        }
+        return $this->assetDir;
     }
 }
