5c1b4770ce1dc8c9b9d3d9e4af16a35ab8e58a53
bubl0id <n.nagornyy@gmail.com>
2020-12-21 17:38:22 +0300

    feat(add block) - add quote

---
+++ app/src/block/vquote/vquote.vue
@@ -0,0 +1,18 @@
+<template>
+  <vue-editor v-model="editorData" :editorToolbar="customToolbar"></vue-editor>
+</template>
+
+<script>
+    import { VueEditor } from "vue2-editor";
+    export default {
+      components:{VueEditor},
+      name: "vquote",
+      data(){
+        return {
+          editorData:"",
+          customToolbar: [["bold", "italic", "underline"]]
+        }
+      }
+    }
+</script>
+
